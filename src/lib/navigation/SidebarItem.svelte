f0146dee168b1678a63547f1d5486ad8b63b875b
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-05 16:30:34 +0200

    Added styling and text for sidebar item

--- src/lib/navigation/SidebarItem.svelte
+++ src/lib/navigation/SidebarItem.svelte
@@ -1,8 +1,41 @@
-<script>
-    import Fa from 'svelte-fa/src/fa.svelte';
-    import { faArrowRightToBracket, faFile, faEnvelope, faHouse, faCircleXmark } from '@fortawesome/free-solid-svg-icons'; 
+<script lang="ts">
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+	export let icon: IconDefinition;
+	export let name: string;
+	export let active = false;
 </script>
 
-<div>
-    <Fa icon={faHouse} />
+<div class={active ? 'active' : ''}>
+	<Fa {icon} size="1.8x" />
+	<p>{name}</p>
 </div>
+
+<style>
+	div {
+		color: var(--myBlue);
+		padding: 1em 0;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 0.5em;
+	}
+
+	div.active {
+		color: white;
+		background-color: var(--myBlue);
+		box-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+	}
+
+	div:hover {
+		cursor: pointer;
+		color: white !important;
+		background-color: var(--myBlue);
+	}
+
+	p {
+		margin: 1em 0 0;
+	}
+</style>
