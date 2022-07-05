d660946f849150bc226c4074b08bb8aa2cce55fb
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-05 15:28:38 +0200

    Created sidebar item with icon

---
+++ src/lib/navigation/SidebarItem.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Fa from 'svelte-fa/src/fa.svelte';
+    import { faArrowRightToBracket, faFile, faEnvelope, faHouse, faCircleXmark } from '@fortawesome/free-solid-svg-icons'; 
+</script>
+
+<div>
+    <Fa icon={faHouse} />
+</div>
