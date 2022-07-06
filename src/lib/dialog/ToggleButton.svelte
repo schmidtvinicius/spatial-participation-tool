7fa40f3ef8f0dd5450079449a181086f6eab8fc8
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-06 18:49:02 +0200

    Created separate toggle button for greeting dialog

---
+++ src/lib/dialog/ToggleButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+	import { createEventDispatcher } from 'svelte';
+	import Fa from 'svelte-fa/src/fa.svelte';
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<button class="toggle-dialog" on:click={() => dispatch('toggle')}>
+	<Fa icon={faQuestion} size="2x" />
+</button>
+
+<style>
+	.toggle-dialog {
+		display: block;
+		border-radius: 50%;
+		border: none;
+		color: white;
+		padding: 1em;
+		width: 4em;
+		height: 4em;
+		align-content: center;
+		background-color: var(--myBlue);
+	}
+</style>
