df91497472383c44b4ff53b565f11f240ecd1f5d
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-06 15:34:04 +0200

    Added string for participate button

--- src/lib/assets/text/strings.ts
+++ src/lib/assets/text/strings.ts
@@ -4,3 +4,4 @@ export const PLACE_GENERAL_PIN = "Place general pin";
 export const EXPLANATION_PARAGRAPH = "We are happy to help you give your opinion about all the different locations around the Netherlands. To do that, enter an access code to participate on a project, or place a general pin anywhere around the country, by clicking '" + PLACE_GENERAL_PIN + "'";
 export const ACCESS_CODE_PLACEHOLDER = "Enter the code";
 export const ACCESS_CODE_LABEL = "Access code";
+export const PARTICIPATE = "Participate";
