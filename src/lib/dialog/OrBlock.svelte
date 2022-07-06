5762506937d46e65858810e371446b19a0d93ee1
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-06 18:48:21 +0200

    Created separate or block component

---
+++ src/lib/dialog/OrBlock.svelte
@@ -0,0 +1,23 @@
+<div class="or-block">
+	<hr />
+	<div class="or-text">or</div>
+	<hr />
+</div>
+
+<style>
+	.or-block {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		padding-bottom: 1em;
+	}
+
+	hr {
+		flex: 1;
+		margin: auto 2em;
+	}
+
+	.or-text {
+		font-family: LatoRegular;
+	}
+</style>
