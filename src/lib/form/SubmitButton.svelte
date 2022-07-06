e5c959fc0210fa3b3b6db6fd60ed312953e501b8
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-06 15:34:15 +0200

    Created submit button

---
+++ src/lib/form/SubmitButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { PARTICIPATE } from '$lib/assets/text/strings';
+	export let content = 'Submit';
+</script>
+
+<button>{content}</button>
+
+<style>
+	button {
+		background-color: var(--myBlue);
+		color: white;
+		width: 100%;
+		border: 0px;
+	}
+</style>
