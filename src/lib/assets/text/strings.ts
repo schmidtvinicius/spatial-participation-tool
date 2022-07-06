1c120771b358e4854c92f69ee62993748e43c04a
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-06 14:29:25 +0200

    Added header, paragraph and input field to greeting dialog. Also added styling

---
+++ src/lib/assets/text/strings.ts
@@ -0,0 +1,6 @@
+export const WELCOME_TEXT = "Welcome to Spatial Quality!";
+export const JOIN_PROJECT = "Join project";
+export const PLACE_GENERAL_PIN = "Place general pin";
+export const EXPLANATION_PARAGRAPH = "We are happy to help you give your opinion about all the different locations around the Netherlands. To do that, enter an access code to participate on a project, or place a general pin anywhere around the country, by clicking '" + PLACE_GENERAL_PIN + "'";
+export const ACCESS_CODE_PLACEHOLDER = "Enter the code";
+export const ACCESS_CODE_LABEL = "Access code";
--- src/lib/dialog/GreetingDialog.svelte
+++ src/lib/dialog/GreetingDialog.svelte
@@ -1,12 +1,36 @@
-<script>
+<script lang="ts">
+	import {
+		WELCOME_TEXT,
+		EXPLANATION_PARAGRAPH,
+		ACCESS_CODE_LABEL,
+		ACCESS_CODE_PLACEHOLDER
+	} from '$lib/assets/text/strings';
+	import TextField from '$lib/form/TextField.svelte';
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faClose } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<div>
-    <h1>BLA</h1>
+<div class="dialog">
+	<div class="action-menu">
+		<Fa icon={faClose} size="2x" />
+	</div>
+	<h1>{WELCOME_TEXT}</h1>
+	<p>{EXPLANATION_PARAGRAPH}</p>
+	<div>
+		<form>
+			<TextField
+				id="access-code"
+				name="access_code"
+				placeholder={ACCESS_CODE_PLACEHOLDER}
+				required={true}
+				label={ACCESS_CODE_LABEL}
+			/>
+		</form>
+	</div>
 </div>
 
 <style>
-	div {
+	.dialog {
 		display: flex;
 		flex-direction: column;
 		height: fit-content;
@@ -15,5 +39,27 @@
 		background-color: #ffffff;
 		box-shadow: var(--myBoxShadow);
 		border-radius: 10px;
+	}
+
+	.action-menu {
+		display: flex;
+		justify-content: flex-end;
+		padding-top: 0.75em;
+	}
+
+	h1 {
+		text-align: center;
+		color: var(--myDarkBlue)
+	}
+
+	p {
+		text-align: justify;
+		color: var(--myDarkBlue)
+	}
+
+	form {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
 	}
 </style>
