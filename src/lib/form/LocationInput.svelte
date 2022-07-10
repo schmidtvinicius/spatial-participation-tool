3ddbc4e1f321b6aa5df860b36d97a5621e98c049
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-10 19:55:13 +0200

    Created location input component with map and text field for the address

---
+++ src/lib/form/LocationInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Map from '$lib/map/Map.svelte';
+	import TextField from './TextField.svelte';
+	import { PIN_ADDRESS_PLACEHOLDER } from '$lib/assets/text/strings';
+</script>
+
+<div id="location-input">
+	<div class="map-container">
+		<Map />
+	</div>
+	<TextField
+		name="Pin address"
+		id="pin-address"
+		placeholder={PIN_ADDRESS_PLACEHOLDER}
+		readOnly={true}
+        label="Address"
+	/>
+</div>
+
+<style>
+	.map-container {
+		height: 50vh;
+		margin-bottom: 1em;
+	}
+</style>
