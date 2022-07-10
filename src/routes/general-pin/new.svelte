63771748c70bc37361cc547f3729ccd2610baae8
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-10 14:43:14 +0200

    Added base form with header

---
+++ src/routes/general-pin/new.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { GENERAL_FORM_HEADER } from "$lib/assets/text/strings";
+</script>
+
+<form>
+    <h1>{GENERAL_FORM_HEADER}</h1>
+</form>
+
+<style>
+    form {
+        position: fixed;
+        left: 30%;
+        top: 10%;
+        display: flex;
+        flex-direction: column;
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: var(--myBoxShadow);
+        width: 50%;
+        height: fit-content;
+        padding: 0.5em 1em;
+    }
+
+    h1 {
+        color: var(--myBlue);
+    }
+</style>
