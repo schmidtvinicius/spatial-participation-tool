0bbe2947913ccc0e65eb5f6a9f4051c6c3b12c77
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-10 19:55:37 +0200

    Started adding components to the general pin form

--- src/routes/general-pin/new.svelte
+++ src/routes/general-pin/new.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
-    import { GENERAL_FORM_HEADER } from "$lib/assets/text/strings";
+	import { GENERAL_FORM_HEADER } from '$lib/assets/text/strings';
+	import LocationInput from '$lib/form/LocationInput.svelte';
+	import TextField from '$lib/form/TextField.svelte';
 </script>
 
-<form>
-    <h1>{GENERAL_FORM_HEADER}</h1>
-</form>
+<div>
+	<form>
+		<h1>{GENERAL_FORM_HEADER}</h1>
+		<section>
+			<h2>Location</h2>
+			<LocationInput />
+		</section>
+		<section>
+			<h2>How do you like this place?</h2>
+			<TextField name="Title" id="pin-title" placeholder="Title" required={true} label={'Title'} />
+		</section>
+	</form>
+</div>
 
 <style>
-    form {
-        position: fixed;
-        left: 30%;
-        top: 10%;
-        display: flex;
-        flex-direction: column;
-        background-color: #fff;
-        border-radius: 10px;
-        box-shadow: var(--myBoxShadow);
-        width: 50%;
-        height: fit-content;
-        padding: 0.5em 1em;
-    }
+	form {
+		position: fixed;
+		left: 30%;
+		top: 10%;
+		display: flex;
+		flex-direction: column;
+		background-color: #fff;
+		border-radius: 10px;
+		box-shadow: var(--myBoxShadow);
+		width: 50%;
+		height: fit-content;
+		padding: 1em;
+	}
 
-    h1 {
-        color: var(--myBlue);
-    }
+	div {
+		height: 100vh;
+	}
+
+	h1,
+	h2 {
+		color: var(--myBlue);
+	}
+
+	section {
+		margin-top: 1em;
+	}
 </style>
