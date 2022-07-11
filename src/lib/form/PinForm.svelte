e7e234d98c443d9091908e392be88b1ed79ef4f8
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-11 10:46:55 +0200

    Created separate component for pin form

---
+++ src/lib/form/PinForm.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { GENERAL_FORM_HEADER } from '$lib/assets/text/strings';
+	import LocationInput from '$lib/form/LocationInput.svelte';
+	import TextField from '$lib/form/TextField.svelte';
+</script>
+
+<form>
+	<h1>{GENERAL_FORM_HEADER}</h1>
+	<section>
+		<h2>Location</h2>
+		<LocationInput />
+	</section>
+	<section>
+		<h2>How do you like this place?</h2>
+		<TextField name="Title" id="pin-title" placeholder="Title" required={true} label={'Title'} />
+	</section>
+</form>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		background-color: white;
+		border-radius: 10px;
+		box-shadow: var(--myBoxShadow);
+		width: 60%;
+        margin: 5em 20%;
+		height: fit-content;
+		padding: 1em;
+	}
+
+	h1,
+	h2 {
+		color: var(--myBlue);
+	}
+
+	section {
+		margin-top: 1em;
+	}
+</style>
