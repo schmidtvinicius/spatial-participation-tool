1e3d266312394df0bb3b5d8f64a88c681c5b88b0
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-11 10:47:22 +0200

    Adjusted styling and positioning of form

--- src/routes/general-pin/new.svelte
+++ src/routes/general-pin/new.svelte
@@ -1,48 +1,19 @@
 <script lang="ts">
-	import { GENERAL_FORM_HEADER } from '$lib/assets/text/strings';
-	import LocationInput from '$lib/form/LocationInput.svelte';
-	import TextField from '$lib/form/TextField.svelte';
+	import PinForm from '$lib/form/PinForm.svelte';
 </script>
 
 <div>
-	<form>
-		<h1>{GENERAL_FORM_HEADER}</h1>
-		<section>
-			<h2>Location</h2>
-			<LocationInput />
-		</section>
-		<section>
-			<h2>How do you like this place?</h2>
-			<TextField name="Title" id="pin-title" placeholder="Title" required={true} label={'Title'} />
-		</section>
-	</form>
+	<PinForm />
 </div>
 
 <style>
-	form {
-		position: fixed;
-		left: 30%;
-		top: 10%;
-		display: flex;
-		flex-direction: column;
-		background-color: #fff;
-		border-radius: 10px;
-		box-shadow: var(--myBoxShadow);
-		width: 50%;
-		height: fit-content;
-		padding: 1em;
-	}
-
 	div {
+		position: fixed;
+		width: 90%;
 		height: 100vh;
-	}
+		left: 10%;
+		top: 0;
+		overflow: scroll;
 
-	h1,
-	h2 {
-		color: var(--myBlue);
-	}
-
-	section {
-		margin-top: 1em;
 	}
 </style>
