09cc66c7f0c58c83aa5844dc300b89f6813c1e5a
Vinicius Schmidt <vinimachado.s@gmail.com>
2022-07-28 18:50:38 +0200

    Added error handling for fetching address

--- src/lib/assets/_external/_addressUtil.ts
+++ src/lib/assets/_external/_addressUtil.ts
@@ -1,3 +1,5 @@
+import { ADDRESS_NOT_FOUND, PIN_OUT_OF_BOUNDS } from '$lib/assets/text/strings';
+
 interface AddressObject {
     road?: string,
     house_number?: string,
@@ -5,7 +7,10 @@ interface AddressObject {
     city?: string,
     town?: string,
     village?: string,
+    country_code?: string
 }
+
+const allowedCountries = ['nl'];
 
 export default async function fetchAddress(lat: number, lng: number): Promise<string> {
     return fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`)
@@ -14,11 +19,12 @@ export default async function fetchAddress(lat: number, lng: number): Promise<st
 }
 
 function parseAddress(fullAddress: AddressObject) {
-    const {road, house_number, postcode, city, town, village} = fullAddress;
+    const { country_code, road, house_number, postcode, city, town, village } = fullAddress;
+    if (!country_code || !allowedCountries.includes(country_code)) throw Error(PIN_OUT_OF_BOUNDS)
     let addressString = '';
     addressString += road ? road + ' ' : '';
     addressString += house_number ? house_number + ', ' : '';
     addressString += postcode ? postcode + ' ' : '';
     addressString += city ?? town ?? village ?? '';
-    return addressString ? addressString : 'Sorry, we kunnen dat adres niet vinden!';
+    return addressString ? addressString : ADDRESS_NOT_FOUND;
 }
